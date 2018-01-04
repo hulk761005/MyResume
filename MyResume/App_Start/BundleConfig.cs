aa02b115aea99393b6d067d12f258a80c175a51e
HulkChou <hulk75321@hotmail.com>
2018-01-04 10:05:59 +0800

    套上 startbootstrap-resume-gh-pages 範本,並更新內容

--- MyResume/App_Start/BundleConfig.cs
+++ MyResume/App_Start/BundleConfig.cs
@@ -20,12 +20,17 @@ namespace MyResume
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Content/vendor/jquery/jquery.min.js",
+                      "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                      "~/Content/vendor/jquery-easing/jquery.easing.min.js",
+                      "~/Scripts/resume.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/Content/vendor/font-awesome/css/font-awesome.css",
+                      "~/Content/vendor/devicons/css/devicons.min.css",
+                      "~/Content/vendor/simple-line-icons/css/simple-line-icons.css",
+                      "~/Content/resume.min.css"));
         }
     }
 }
